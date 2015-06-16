f83321951796b3408188b170ec67aade1680ae06
timdenison <tim.denison@gmail.com>
2015-06-15 17:18:47 -0700

    First commit
    A fictitious spy agency needs to assign spies by code name to named
    assignments. Assignments have a start and end date and cost is 500
    dollars per day with a 1000 dollar bonus if assignment length exceeds 21
    days. Start date cannot be within 14 days of the end of spy's previous
    assignment end date.

---
+++ ChallengeEpicSpiesAssignment/Default.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeEpicSpiesAssignment.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            
+            height: 190px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <img alt="Epic Spies company logo" class="auto-style1" src="epic-spies-logo.jpg" /><br />
+        <br />
+        Spy New Assignment Form<br />
+        <br />
+        Spy Code Name:
+        <asp:TextBox ID="codeNameTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        New Assignment Name:
+        <asp:TextBox ID="assignmentNameTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        End Date of previous assignment:<br />
+        <asp:Calendar ID="prevEndDateCalendar" runat="server"></asp:Calendar>
+        <br />
+        Start Date of New Assignment:<br />
+        <asp:Calendar ID="startDateCalendar" runat="server"></asp:Calendar>
+        <br />
+        Projected End Date of New Assignment:<br />
+        <asp:Calendar ID="endDateCalendar" runat="server"></asp:Calendar>
+        <br />
+        <asp:Button ID="assignSpyButton" runat="server" OnClick="assignSpyButton_Click" Text="Assign Spy" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeEpicSpiesAssignment/Default.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeEpicSpiesAssignment
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                prevEndDateCalendar.SelectedDate = DateTime.Now.Date;
+                startDateCalendar.SelectedDate = prevEndDateCalendar
+                    .SelectedDate.AddDays(14);
+                endDateCalendar.SelectedDate = startDateCalendar
+                    .SelectedDate.AddDays(7);
+            }
+          
+        }
+
+        protected void assignSpyButton_Click(object sender, EventArgs e)
+        {
+            string codeName = codeNameTextBox.Text;
+            string assignmentName = assignmentNameTextBox.Text;
+
+            DateTime availDate = prevEndDateCalendar.SelectedDate;
+            DateTime startDate = startDateCalendar.SelectedDate;
+            DateTime endDate = endDateCalendar.SelectedDate;
+
+            int restLength = startDate.Subtract(availDate).Days;
+            int assignmentLength = endDate.Subtract(startDate).Days;
+            int earnings;
+
+            if (restLength < 14)
+            {
+                resultLabel.Text = "Error: Assignment start date must " +
+                    "be at least 14 days from previous assignment end date.";
+                startDateCalendar.SelectedDate = availDate.AddDays(14);
+                startDateCalendar.VisibleDate = startDateCalendar.SelectedDate;
+                endDateCalendar.SelectedDate = startDateCalendar.SelectedDate.AddDays(7);
+                endDateCalendar.VisibleDate = endDateCalendar.SelectedDate;
+            }
+            else if(codeName == "" || assignmentName == "")
+            {
+                resultLabel.Text = "Error: Please enter a Spy name and Assignment name.";
+            }
+            else
+            {
+                if (assignmentLength <= 21)
+                {
+                    earnings = 500 * assignmentLength;
+                }
+                else
+                {
+                    earnings = 500 * assignmentLength + 1000;
+                }
+
+                resultLabel.Text = String.Format("Assignment of <i>{0}</i>" +
+                    " to the <b>{1}</b> assignment is authorized. Budget" +
+                    " total: {2:C}", codeName, assignmentName, earnings);
+            }
+
+            
+
+
+        }
+    }
+}
